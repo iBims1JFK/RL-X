6efe194c3bb0d14053a395c60e12adbbaed6d435
Nico <nico.bohlinger@gmail.com>
2025-01-20 16:34:09 +0100

    crossq2 template

---
+++ rl_x/algorithms/crossq2/flax/__init__.py
@@ -0,0 +1,8 @@
+from rl_x.algorithms.algorithm_manager import extract_algorithm_name_from_file, register_algorithm
+from rl_x.algorithms.crossq2.flax.crossq import CrossQ2
+from rl_x.algorithms.crossq2.flax.default_config import get_config
+from rl_x.algorithms.crossq2.flax.general_properties import GeneralProperties
+
+
+CROSSQ2_FLAX = extract_algorithm_name_from_file(__file__)
+register_algorithm(CROSSQ2_FLAX, get_config, CrossQ2, GeneralProperties)
